92d4eb3de30cda6ed1a5a5b1e756f1eb780c2ecb
Steve Blurr <stevenalopez@outlook.com>
2022-05-24 20:12:19 -0700

    added env variable capabilities and python requirements

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 import urllib, hashlib
+import os
 
 app = Flask(__name__)
 login_manager = LoginManager()
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 gravatar = Gravatar(app,
                     size=100,
@@ -27,7 +28,7 @@ login_manager.init_app(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
